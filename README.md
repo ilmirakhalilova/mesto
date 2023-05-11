03cf7902ddf15da477a70e8591e59a9376074f39
ilmira khalilova <mirakhaleesi@yandex.ru>
2023-05-11 05:19:52 +0300

    fixed bags after first review

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Проект: Место
+# Проект: Mesto
 
-https://ilmirakhalilova.github.io/mesto/
+[Ссылка на GitHub](https://ilmirakhalilova.github.io/mesto/)
 
 ### Обзор
 * Описание
@@ -8,15 +8,15 @@ https://ilmirakhalilova.github.io/mesto/
 
 **Описание**
 
-Был реализован проект Mesto от ЯндексПрактикума по макету в Figma для разных устройств.
+Одностраничный сайт от ЯндексПрактикума с адаптивной версткой, возможностью открытия и закрытия popup и внесения изменений на странице:
+- редактирование профиля
+- отметка карточки лайком
+- добавление новых карточек
+- удаление карточек
+
 [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
 
 **Инструменты и технологии**
-* JavaScript- первое знакомство
-* Figma
-* Flexbox
-* Grid Layout
-* https://tinypng.com/ - оптимизация изображений
-* https://rsms.me/inter/ - подключение кастомных шрифтов
-
-Файловая структура реализована по БЭМ.
+* HTML
+* CSS
+* JS
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,10 @@
 //ПЕРЕМЕННЫЕ
-const popup = document.querySelector('.popup');
-
 //Попап редактирование профиля
-const editButtonLink = document.querySelector('.profile__edit-button');
+const editPopupOpenButton = document.querySelector('.profile__edit-button');
 const editPopup = document.querySelector('.popup_edit');
 const editPopupCloseButton = editPopup.querySelector('.popup__close_edit');
 const nameInput = editPopup.querySelector('.popup__input_type_name');
 const statusInput = editPopup.querySelector('.popup__input_type_status');
-const editPopupSubmit = editPopup.querySelector('.popup__submit');
 const editPopupForm = editPopup.querySelector('.popup__form');
 const personName = document.querySelector('.profile__name');
 const personStatus = document.querySelector('.profile__status');
@@ -17,8 +14,8 @@ const addButtonLink = document.querySelector('.profile__add-button');
 const addPopup = document.querySelector('.popup_add');
 const addPopupForm = addPopup.querySelector('.popup__form');
 const addPopupCloseButton = addPopup.querySelector('.popup__close_add');
-const nameadd = addPopup.querySelector('.popup__input_new_place');
-const linkadd = addPopup.querySelector('.popup__input_link_place');
+const nameAdd = addPopup.querySelector('.popup__input_new_place');
+const linkAdd = addPopup.querySelector('.popup__input_link_place');
 
 //Темплейт с карточками
 const cardsTemplate = document.getElementById('cards-template');
@@ -30,34 +27,6 @@ const popupImage = document.querySelector('.popup_image');
 const image = document.querySelector('.popup__image');
 const imageCaption = document.querySelector('.popup__image-caption');
 const imagePopupCloseButton = popupImage.querySelector('.popup__close_image');
-
-//Данные
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 
 //ФУНКЦИИ
@@ -77,20 +46,20 @@ function openFormForEdit () {
   nameInput.value = personName.textContent;
   statusInput.value = personStatus.textContent;
 }
-editButtonLink.addEventListener('click', openFormForEdit);
+editPopupOpenButton.addEventListener('click', openFormForEdit);
 
 function closeEditPopup () {
   closePopup(editPopup);
 }
 editPopupCloseButton.addEventListener('click', closeEditPopup);
 
-function handleFormSubmit (event) {
+function handleEditFormSubmit (event) {
   event.preventDefault();
   personName.textContent = nameInput.value;
   personStatus.textContent = statusInput.value;
   closeEditPopup ();
 }
-editPopupForm.addEventListener('submit', handleFormSubmit);
+editPopupForm.addEventListener('submit', handleEditFormSubmit);
 
 //Функции для формы добавления в галерею
 function openFormForAdd () {
@@ -106,8 +75,8 @@ addPopupCloseButton.addEventListener('click', closeAddPopup);
 function createNewCard(event){
   event.preventDefault();
   const manuallCardData = {
-    name: nameadd.value,
-    link: linkadd.value
+    name: nameAdd.value,
+    link: linkAdd.value
   };
   cardsContainer.prepend(createCard(manuallCardData));
   closeAddPopup();
@@ -141,27 +110,20 @@ const createCard = (cardData) => {
   //попап большой картинки
   cardImage.addEventListener('click', () => {
     openPopup(popupImage);
-    image.src = cardImage.src;
-    image.alt = cardName.textContent;
-    imageCaption.textContent = cardName.textContent;
+    image.src = cardData.link;
+    image.alt = cardData.name;
+    imageCaption.textContent = cardData.name;
   });
-  function closePopupImage (){
-    closePopup(popupImage);
-  };
-  imagePopupCloseButton.addEventListener('click', closePopupImage);
+  
   return cardElement;
 };
+
+function closePopupImage (){
+  closePopup(popupImage);
+};
+imagePopupCloseButton.addEventListener('click', closePopupImage);
 
 initialCards.forEach((card) => {
   const element = createCard(card);
   cardsContainer.prepend(element);
 });
-
-
-
-
-
-
-
-
-
