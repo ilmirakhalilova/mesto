24e016f88dadd58344104b9836a15d5915224334
ilmira khalilova <mirakhaleesi@yandex.ru>
2023-06-27 03:52:17 +0300

    ПР8

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import Card from '../components/Card.js';
-import {initialCards, validationConfig} from '../components/constants.js';
-import FormValidator from '../components/FormValidator.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import UserInfo from '../components/UserInfo.js';
-import Section from '../components/Section.js';
+import Card from './components/Card.js';
+import {initialCards, validationConfig} from './components/constants.js';
+import FormValidator from './components/FormValidator.js';
+import PopupWithImage from './components/PopupWithImage.js';
+import PopupWithForm from './components/PopupWithForm.js';
+import UserInfo from './components/UserInfo.js';
+import Section from './components/Section.js';
+import './pages/index.css'; //импорт главного файла стилей 
 
 //ПЕРЕМЕННЫЕ
 //Попап редактирование профиля
